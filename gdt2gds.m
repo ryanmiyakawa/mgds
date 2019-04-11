8df90c13307d4278a1060a368c0c66de00cd4328
Ryan-Miyakawa <ryan@hooktheory.com>
2019-04-10 17:06:22 -0700

    cleaning up mgds

--- gdt2gds.m
+++ gdt2gds.m
@@ -1,20 +1,7 @@
 function gdt2gds(gdtName, gdsName, deleteFlag)
 
 d = pwd;
-ver = 'debug';
-
-switch ver
-    case 'debug'
-        str = sprintf('/Users/rhmiyakawa/Documents/MATLAB/GDS/old/GDS_source/GDT-1.0.1_RMEdited_working/gdt2gds.Darwin %s/%s %s/%s', d, gdtName, d,gdsName);
-    case 'v3'
-        str = sprintf('/Users/rhmiyakawa/Documents/MATLAB/GDS/MGDS/gdt2gds_v3.Darwin %s/%s %s/%s', d, gdtName, d,gdsName);
-    case '2007'
-        str = sprintf('/Users/rhmiyakawa/Documents/MATLAB/GDS/MGDS/gdt2gds_2007.Darwin %s/%s %s/%s', d, gdtName, d,gdsName);
-    case '1.1'
-        str = sprintf('/Users/rhmiyakawa/Documents/MATLAB/GDS/MGDS/gdt2gdsRM.Darwin %s/%s %s/%s', d, gdtName, d,gdsName);
-    case '1.1pure'
-        str = sprintf('/Users/rhmiyakawa/Documents/MATLAB/GDS/MGDS/gdt2gds_pure.Darwin %s/%s %s/%s', d, gdtName, d,gdsName);
-end
+str = sprintf('gdt2gdsRM.Darwin %s/%s %s/%s', d, gdtName, d,gdsName);
 
 system(str);
 
